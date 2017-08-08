77d1e27707ee43de131f158743dbc28d5ccf4d74
Nathaniel Rudavsky-Brody <nathaniel.rudavsky@tenforce.com>
2017-08-08 11:50:08 +0200

    cleanup

--- triples.scm
+++ triples.scm
@@ -7,7 +7,6 @@
 (include "fmicroKanren/fmicroKanren.scm")
 (include "fmicroKanren/miniKanren-wrappers.scm")
 
-
 (define-syntax project
   (syntax-rules ()
     ((_ (x ...) g)
@@ -17,9 +16,19 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Database
-(define-record db time tables)
+;; tied together with lists, persistent hash maps, and shoestring
+;;
+;; basic structure, []'s are persistent hash maps and <>'s are records
+;; <database
+;;   :time = (t0 t1 t2 ...)
+;;   :indexes = [ t0 => <index :s  [<S> => (<P1> <P2> ...)  ]
+;;                             :sp [(<S> <P1>) => (O11 O12 ...)  ]
+;;                             ...>
+;;                t1 => ... ] 
+;; >
+(define-record db time indexes)
 
-(define-record dbs s sp p po o os spo)
+(define-record index s sp p po o os spo)
 
 (define (empty-index) (persistent-map))
 
@@ -29,48 +38,48 @@
      (list time)
      (persistent-map
       time
-      (make-dbs (empty-index) (empty-index) (empty-index) (empty-index)
-                (empty-index) (empty-index) (empty-index))))))
+      (apply make-index (make-list 7 (empty-index)))))))
 
 (define *DB* (make-parameter (empty-db)))
 
-(define (latest DB)
-  (map-ref (db-tables DB) (car (db-time DB))))
+(define (latest-db)
+  (map-ref (db-indexes (*DB*)) (car (db-time (*DB*)))))
 
 (define (appendor a b)
   (append (or a '()) (or b '())))
 
-;; !! **
-(define (consr lst elt)
+(define (consr lst elt) ;; shoestring...
   (if (and (list? lst) (member elt lst)) lst
       (cons elt (or lst '()))))
 
+(define index-key list)
+
 (define (update-triples triples val)
   (let* ((time (cpu-time))
-        (DBS (latest (*DB*))))
+        (DBS (latest-db)))
     (let loop ((triples triples)
-               (si (dbs-s DBS))
-               (spi (dbs-sp DBS))
-               (pi (dbs-p DBS))
-               (poi (dbs-po DBS))
-               (oi (dbs-o DBS))
-               (osi (dbs-os DBS))
-               (spoi (dbs-spo DBS)))
+               (si (index-s DBS))
+               (spi (index-sp DBS))
+               (pi (index-p DBS))
+               (poi (index-po DBS))
+               (oi (index-o DBS))
+               (osi (index-os DBS))
+               (spoi (index-spo DBS)))
       (if (null? triples)
           (*DB*
            (make-db
             (cons time (db-time (*DB*)))
-            (map-add (db-tables (*DB*)) time (make-dbs si spi pi poi oi osi spoi))))
+            (map-add (db-indexes (*DB*)) time (make-index si spi pi poi oi osi spoi))))
           (match (car triples)
             ((s p o)
              (loop (cdr triples)
                    (map-update-in si `(,s) consr p)
-                   (map-update-in spi `(,(symbol-append s p)) consr o)
+                   (map-update-in spi `(,(index-key s p)) consr o)
                    (map-update-in pi `(,p) consr  o)
-                   (map-update-in poi `(,(symbol-append p o)) consr s)
+                   (map-update-in poi `(,(index-key p o)) consr s)
                    (map-update-in oi `(,o) consr s)
-                   (map-update-in osi `(,(symbol-append o s)) consr p)
-                   (map-add spoi (symbol-append s p o) val))))))))
+                   (map-update-in osi `(,(index-key o s)) consr p)
+                   (map-add spoi (index-key s p o) val))))))))
 
 (define add-triples (cut update-triples <> #t))
 
@@ -83,36 +92,35 @@
   (delete-triples `((,s ,p ,o))))
 
 (define (triple-nol s p o minus)
-  (let ((I (lambda (table key)
-                   (lambda (db)
-                     (map-ref (table db) key))))
-        (T (lambda (indexes var)
-	     (let ((vals (indexes (latest (*DB*)))))
-	       (let stream ((ref '()) (next-ref vals) (os vals))
+  (let ((index-getter (lambda (table key)
+                        (lambda () (map-ref (table (latest-db)) key))))
+        (T (lambda (index var)
+	     (let ((vals (index)))
+	       (let stream ((os vals) (ref '()) (next-ref vals))
 		 (if (equal? os ref)
 		     (later
-		      (let ((vals (indexes (latest (*DB*)))))
-                        (stream next-ref vals vals)))
+		      (let ((vals (index)))
+                        (stream vals next-ref vals)))
 		     (disj
 		      (conj (== var (car os))
 			    (project (s p o) (triple-nol s p o minus)))
-		      (stream ref next-ref (cdr os)))))))))
-    (cond ((and (var? s) (var? p) (var? o)) (T (compose map-keys dbs-s) s))
-	  ((and (var? s) (var? p))          (T (I dbs-o o) s))
-	  ((and (var? s) (var? o))          (T (I dbs-p p) o))
-	  ((and (var? p) (var? o))          (T (I dbs-s s) p))
-	  ((var? s)                         (T (I dbs-po (symbol-append p o)) s))
-	  ((var? p)                         (T (I dbs-os (symbol-append o s)) p))
-	  ((var? o)                         (T (I dbs-sp (symbol-append s p)) o))
+		      (stream (cdr os) ref next-ref))))))))
+    (cond ((and (var? s) (var? p) (var? o)) (T (compose map-keys index-s) s))
+	  ((and (var? s) (var? p))          (T (index-getter index-o o) s))
+	  ((and (var? s) (var? o))          (T (index-getter index-p p) o))
+	  ((and (var? p) (var? o))          (T (index-getter index-s s) p))
+	  ((var? s)                         (T (index-getter index-po (index-key p o)) s))
+	  ((var? p)                         (T (index-getter index-os (index-key o s)) p))
+	  ((var? o)                         (T (index-getter index-sp (index-key s p)) o))
 	  (else
-           (let ((val (lambda () (map-ref (dbs-spo (latest (*DB*))) (symbol-append s p o))))
-                 (sign (lambda (v) (if v '+ '-))))
-                (let singleton ((ref #f))
-                  (let ((v (val)))
-                    (cond ((and v ref) (later (singleton v)))
-                          (v (disj (== #t #t) (later (singleton v))))
-                          (else (disj (== minus '-) (later (singleton v))))))))))))
+           (let singleton ((ref #f))
+             (let ((v ((index-getter index-spo (index-key s p o)))))
+               (cond ((eq? v ref) (later (singleton v)))
+                     (v (disj (== #t #t) (later (singleton v))))
+                     (else (disj (== minus '-) (later (singleton v)))))))))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Tests
 (add-triples '((<S> <P> <O>)
 	       (<U> <V> <O>)
 	       (<S> <P> <O2>)
@@ -127,15 +135,16 @@
 
 (print r)
 
+(delete-triples '((<S> <P> <O>)))
+
+(print (future r))
+
 (add-triples '((<S> <P> <O3>)
+               (<S> <P> <O>)
 	       (<Q> <R> <O3>)
 	       (<U> <V> <O3>)
 	       (<S> <P> <M>)
 	       (<U> <V> <M>)
 	       (<Q> <R> <M>)))
-
-(print (future r))
-
-(delete-triples '((<S> <P> <O>)))
 
 (print (future (future r)))
